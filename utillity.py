9018c6f1dad6df815d17f2cf1849e1ec5197a0ad
codeinstant <codeinstan@gmail.com>
2023-12-01 20:54:30 +0700

    del

--- utillity.py
+++
@@ -1,7 +0,0 @@
-def onselect(evt):
-    # Note here that T kinter passes an event object to onselect()
-    w = evt.widget
-    index = int(w.curselection()[0])
-    value = w.get(index)
-    print('You selected item %d: "%s"' % (index, value))
-        
